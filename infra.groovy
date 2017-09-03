0958e7ed14e085f9165073e4f18b5c02d7967587
Joshua Copeland <joshuarcopeland@gmail.com>
2017-09-02 17:30:10 -0700

    try this for fix groovy loc

--- infra.groovy
+++ infra.groovy
@@ -1,15 +1,29 @@
 import hudson.model.*
 
 node {
+    deleteDir()
+
+    def groovyScriptsPath = 'jenkins-pipeline-groovy'
+
+    // This is some stupid shit but groovy pipeline won't see other groovy scripts so you gotta do this mickey mouse
+    stage('bootstrap') {
+        dir(groovyScriptsPath) {
+            sh """
+                git clone git@github.com:OGProgrammer/jenkins-pipeline-groovy.git --branch=master .
+            """
+        }
+    }
+
 
     stage('prep') {
-        deleteDir()
         def env_name = "${env_name}"
         def region = "${region}"
         def action = "${action}"
         println "Preforming [${action}] with AWS infrastrucutre [${env_name}] in the [${region}] region."
 
-        functions = load("${WORKSPACE}/functions.groovy")
+        dir(groovyScriptsPath) {
+            functions = load("functions.groovy")
+        }
 
         // Build the terraform vars
         tfVarsFileData = ''
